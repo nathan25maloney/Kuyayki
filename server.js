4ad425e53c20b0a789c108505e534b8c2cef1c7d
Nathan Maloney <nathan25maloney@gmail.com>
2019-04-02 18:34:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-const flash = require("connect-flash");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const logger = require("morgan");
+// const flash = require("connect-flash");
+// const cookieParser = require("cookie-parser");
+// const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("assets"));
 
-app.use(cookieParser());
-app.use(logger("dev"));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(cors());
-app.use(flash());
+// app.use(cookieParser());
+// app.use(logger("dev"));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// app.use(cors());
+// app.use(flash());
 
 app.listen(PORT, function() {
     console.log(`App listening on http://localhost:${PORT}`);
